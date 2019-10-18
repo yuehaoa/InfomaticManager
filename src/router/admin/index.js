bab4c84b79834495740a91006a26fb40287d70c8
sh963852741 <sh963852741@126.com>
2019-10-18 18:59:15 +0800

    创建新页面

--- src/router/admin/index.js
+++ src/router/admin/index.js
@@ -1,2 +1,12 @@
 module.exports = {
+    BuildingManager: {
+        path: "/manage/building/buildingmanage",
+        layout: "admin",
+        component: () => import(/* webpackChunkName: "itemmanager" */"@p/Modules/BuildingManage")
+    },
+    LabManager: {
+        path: "/manage/building/labmanage",
+        layout: "admin",
+        component: () => import(/* webpackChunkName: "itemmanager" */"@p/Modules/LabManage")
+    }
 };
